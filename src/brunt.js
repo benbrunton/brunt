686a525fbf724714b9ba8ee8d466cdf442339da0
Ben Brunton <ben.b.brunton@gmail.com>
2021-09-21 23:04:17 +0100

    update default settings on brunt creation

--- src/brunt.js
+++ src/brunt.js
@@ -4,11 +4,11 @@ import { keys, touch } from './constants.js';
 const defaultSettings = {
     width: 400,
     height: 400,
-    frameLimit: 1000 / 60,
-    blockSize: 10
+    frameLimit: 1000 / 60
 };
 
-const brunt = (settings = defaultSettings) => {
+const brunt = (settings) => {
+    const combinedSettings = {...defaultSettings, ...settings};
     return createGame(settings);
 };
 
